0c93fc74905cf355effb8647efbb1551107d3564
Xavier Graham <xavierwg13@gmail.com>
2024-06-17 20:31:21 -0700

    Refactor for stats display
    stats that use luck are refactored with Utils.cs file

---
+++ LookingGlass/Utils.cs
@@ -0,0 +1,28 @@
+﻿using RoR2;
+using System;
+using UnityEngine;
+
+namespace LookingGlass
+{
+    class Utils
+    {
+        public static float CalculateChance(float baseChance, float luckIn)
+        {
+            int luck = Mathf.CeilToInt(luckIn);
+            if (luck > 0)
+                return 1f - Mathf.Pow(1f - baseChance, luck + 1);
+            if (luck < 0)
+                return Mathf.Pow(baseChance, Mathf.Abs(luck) + 1);
+
+            return baseChance;
+        }
+
+        public static float GetLuckFromCachedUserBody(CharacterBody cachedUserBody)
+        {
+            // Check for modded luck values
+            if (cachedUserBody && cachedUserBody.master) return cachedUserBody.master.luck;
+
+            return cachedUserBody.inventory.GetItemCount(RoR2Content.Items.Clover) - cachedUserBody.inventory.GetItemCount(RoR2Content.Items.LunarBadLuck);
+        }
+    }
+}
